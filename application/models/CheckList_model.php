8fcfd373e16efacacfb3c3b8e7228b19acc1b03e
jaimechaconarevalo <41206411+jaimechaconarevalo@users.noreply.github.com>
2021-09-14 20:41:25 -0300

    se sube cambios responsive en la tabla de agregar Inspeccion

--- application/models/CheckList_model.php
+++ application/models/CheckList_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class CheckList_model extends CI_Model
+class Checklist_model extends CI_Model
 {
 	public function __construct()
 	{
